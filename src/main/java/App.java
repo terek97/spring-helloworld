8bba654d5bc0aeb644a2437deab7512515efd91b
terek97 <OiL1119002OiL>
2021-05-15 01:09:47 +0400

    first solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld hWBean = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == hWBean);
+        System.out.println(cat == catBean);
     }
 }
